8f6552d66a4def024313cab4abc4558b6aea43a3
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-28 00:46:09 +0500

    Errors in Typescript

---
+++ index.ts
@@ -0,0 +1,33 @@
+// Errors 
+
+// Type Error
+
+let message: string = "Hello World"
+// console.loged(message) // this is type error
+console.log(message);
+
+
+// Syntax Error
+
+// coonst name1: string = "areeba" // this is syntax error
+const name1: string = "Areeba";
+console.log(name1);
+
+
+
+// Assignability Error
+
+let age: number = 22;
+// age = "Twenty Two"; // this is assignabilty error
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
